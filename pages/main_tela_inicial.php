df654233ec51e085f838c87824dc01a9f0f754a8
Igor <igoralbieri04@gmail.com>
2024-04-09 11:31:48 -0300

    aaaa

---
+++ pages/main_tela_inicial.php
@@ -0,0 +1,25 @@
+<?php
+
+include_once("../includes/menu.php");
+
+?>
+<link rel="stylesheet" href="../assets/css/tela_inicial.css">
+<title>tela_inicial</title>
+<body>
+    
+    <section class="area-main">
+        <div class="card1">
+            <div class="notiglow1"></div>
+            <div class="notiborderglow1"></div>
+            <div class="notititle1">Welcome To Uiverse</div>
+            <div class="notibody1">Contribute to Open Source UI Elements</div>
+        </div>
+
+
+
+
+
+    </section>
+
+</body>
+</html>
