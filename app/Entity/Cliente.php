0255636ca92275ace32f1e41e03059c29ba04667
Igor <igoralbieri04@gmail.com>
2024-04-11 14:21:26 -0400

    amem

---
+++ app/Entity/Cliente.php
@@ -0,0 +1,121 @@
+<?php
+
+namespace App\Entity;
+
+use \App\Db\Database;
+use \PDO;
+
+
+class Cliente{
+
+    /** 
+     *Indentificador Unico do cliente 
+     *@var integer
+    */
+    public $id_cli;  
+    
+    /** 
+     *Nome do cliente 
+     *@var string
+    */
+    public $nome;  
+
+    /** 
+    *Codigo do cliente 
+    *@var string
+    */
+    public $codigo; 
+
+    /** 
+    *Cnpj do cliente 
+    *@var string
+    */
+    public $cnpj;
+    
+    /** 
+    *Telefone do cliente 
+    *@var string
+    */
+    public $telefone;
+    
+    /** 
+    *País do cliente 
+    *@var string
+    */
+    public $pais; 
+        
+    /** 
+    *Cep do cliente 
+    *@var string
+    */
+    public $cep;
+        
+    /** 
+    *Cidade do cliente 
+    *@var string
+    */
+    public $cidade;
+        
+    /** 
+    *Estado do cliente 
+    *@var string
+    */
+    public $estado;
+        
+    /** 
+    *Numero do cliente 
+    *@var string
+    */
+    public $numero;
+        
+    /** 
+    *Rua do cliente 
+    *@var string
+    */
+    public $rua;
+        
+    /** 
+    *Bairro do cliente 
+    *@var string
+    */
+    public $bairro;
+    
+        
+    /** 
+    *observação do cliente 
+    *@var string
+    */
+    public $observacao; 
+
+
+    /** 
+     *Método responsavel por cadastrar uma nova categoria no banco 
+     *@return boolean
+    */
+
+
+    public function cadastrar(){
+
+            $objDatabase = new Database('cliente');
+            $this->id = $objDatabase->insert([
+                'codigo' => $this->codigo,
+                'nome' => $this->nome,
+                'telefone' => $this->telefone,
+                'cnpj' => $this->cnpj,
+                'pais' => $this->pais,
+                'cep' => $this->cep,
+                'cidade' => $this->cidade,
+                'estado' => $this->estado,
+                'numero' => $this->numero,
+                'rua' => $this->rua,
+                'bairro' => $this->bairro,
+                'observacao' => $this->observacao,
+            ]);
+           
+            return true;
+
+    }
+
+
+
+}
--- pages/main_cad_cli.php
+++ pages/main_cad_cli.php
@@ -5,8 +5,29 @@ require_once __DIR__ . '/../vendor/autoload.php';
 
 include_once("../includes/menu.php");
 
-use App\WebService\ViaCep;
+use App\Entity\Cliente;
 
+if (isset($_POST["codigo"], $_POST["nome"], $_POST["telefone"], $_POST["cnpj"], $_POST["pais"], $_POST["cep"], $_POST["cidade"], $_POST["estado"], $_POST["numero"], $_POST["rua"], $_POST["bairro"], $_POST["obs"])) {
+
+    $objcliente = new Cliente();
+
+    
+    $objcliente->codigo = $_POST["codigo"];
+    $objcliente->nome = $_POST["nome"];
+    $objcliente->telefone = $_POST["telefone"];
+    $objcliente->cnpj = $_POST["cnpj"];
+    $objcliente->pais = $_POST["pais"];
+    $objcliente->cep = $_POST["cep"];
+    $objcliente->cidade = $_POST["cidade"];
+    $objcliente->estado = $_POST["estado"];
+    $objcliente->numero = $_POST["numero"];
+    $objcliente->rua = $_POST["rua"];
+    $objcliente->bairro = $_POST["bairro"];
+    $objcliente->observacao = $_POST["obs"];
+    
+
+    $objcliente->cadastrar();
+}
 
 ?>
 <link rel="stylesheet" href="../assets/css/cad_cli.css">
@@ -16,28 +37,28 @@ use App\WebService\ViaCep;
     
     <section class="area-main">
         <span id="message"></span>
-        <form class="area-form" id="form-cli" action="">
+        <form class="area-form" id="form-cli" method="POST" action="">
             
             <h1 id="titulo_page">Cadastrar Cliente</h1>
             
             <div class="input-field">
-                <input required="" class="input" type="text" />
+                <input required="" class="input" name="codigo" type="text" />
                     <label class="label" for="input">Codigo</label>
             </div>
             
             <div class="input-field">
-                <input required="" class="input" type="text" />
+                <input required="" class="input"name="nome" type="text" />
                 <label class="label" for="input">Digite o nome</label>
             </div>
             
             <div class="juntar-input">
                 <div class="input-field">
-                    <input required="" class="input" type="text" />
+                    <input required="" class="input" name="cnpj" type="text" />
                     <label class="label" for="input">Digite o cnpj</label>
                 </div>
                 
                 <div class="input-field">
-                    <input required="" class="input" type="text" />
+                    <input required="" class="input" name="telefone" type="text" />
                     <label class="label" for="input">Digite o telefone</label>
                 </div>
             </div>
@@ -47,7 +68,7 @@ use App\WebService\ViaCep;
             <div class="select-field">
                 
                 <label id="label-txt" for="">País</label>            
-                <select class="select">
+                <select class="select" name="pais">
                     <option>Brasil</option>
                     <option>Chile</option>
                     <option>Paraguay</option>
@@ -74,7 +95,7 @@ use App\WebService\ViaCep;
 
 
                 <div class="input-field">
-                    <input required="" class="input" type="text"/>
+                    <input required="" class="input" name="numero" type="text"/>
                     <label class="label" for="input">Numero</label>
                 </div>
                 
@@ -96,7 +117,7 @@ use App\WebService\ViaCep;
             </div>
 
             <div class="input-field">
-                    <input required="" class="input" type="text" />
+                    <input required="" class="input" type="text" name="obs"/>
                     <label class="label" for="input">observação</label>
             </div>
             
