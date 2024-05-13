e8670bc3d59c16ce8479e8d2aadad7041d215fd4
Igor <igoralbieri04@gmail.com>
2024-05-13 11:19:45 -0400

    a

--- includes/php/cad_perf.php
+++ includes/php/cad_perf.php
@@ -2,11 +2,29 @@
 
 use App\Entity\Perfil;
 
-if (isset($_POST["nome"])) {
+if (isset($_POST["nome"]) && isset($_POST["cadastrar_chamado"]) && isset($_POST["todas_os"]) && isset($_POST["requisicao_chamado"]) && isset($_POST["relatorio_chamados"]) && isset($_POST["aceitar_recusar_chamado"]) && isset($_POST["editar_chamado"]) && isset($_POST["relatorio_os"]) && isset($_POST["responder_os"]) && isset($_POST["aceitar_recusar_os"]) && isset($_POST["editar_os"]) && isset($_POST["cadastrar_usuario"]) && isset($_POST["cadastrar_perfil"]) && isset($_POST["cadastrar_item"]) && isset($_POST["cadastrar_cliente"]) && isset($_POST["gerenciar_usuario"]) && isset($_POST["gerenciar_perfil"]) && isset($_POST["gerenciar_item"]) && isset($_POST["gerenciar_cliente"])) {
 
     $objperfil = new Perfil();
 
     $objperfil->nome = $_POST["nome"];
+    $objperfil->cadastrar_chamado = $_POST["cadastrar_chamado"];
+    $objperfil->todas_os = $_POST["todas_os"];
+    $objperfil->requisicao_chamado = $_POST["requisicao_chamado"];
+    $objperfil->relatorio_chamados = $_POST["relatorio_chamados"];
+    $objperfil->aceitar_recusar_chamado = $_POST["aceitar_recusar_chamado"];
+    $objperfil->editar_chamado = $_POST["editar_chamado"];
+    $objperfil->relatorio_os = $_POST["relatorio_os"];
+    $objperfil->responder_os = $_POST["responder_os"];
+    $objperfil->aceitar_recusar_os = $_POST["aceitar_recusar_os"];
+    $objperfil->editar_os = $_POST["editar_os"];
+    $objperfil->cadastrar_usuario = $_POST["cadastrar_usuario"];
+    $objperfil->cadastrar_perfil = $_POST["cadastrar_perfil"];
+    $objperfil->cadastrar_item = $_POST["cadastrar_item"];
+    $objperfil->cadastrar_cliente = $_POST["cadastrar_cliente"];
+    $objperfil->gerenciar_usuario = $_POST["gerenciar_usuario"];
+    $objperfil->gerenciar_perfil = $_POST["gerenciar_perfil"];
+    $objperfil->gerenciar_item = $_POST["gerenciar_item"];
+    $objperfil->gerenciar_cliente = $_POST["gerenciar_cliente"];
 
     $objperfil->cadastrar();
 
--- pages/main_cad_perf.php
+++ pages/main_cad_perf.php
@@ -7,186 +7,170 @@ include_once ("../includes/php/cad_perf.php");
 
 ?>
 <link rel="stylesheet" href="../assets/css/cad_perf.css">
-<title>Cadatrar Perfil</title>
+<title>Cadastrar Perfil</title>
 
 <body>
 
-    <body>
+    <section class="area-main">
 
-        <section class="area-main">
+        <form class="area-form" method="POST" action="">
 
-            <form class="area-form" method="POST" action="">
+            <h1 id="titulo_page">Cadastrar Perfil</h1>
 
-                <h1 id="titulo_page">Cadastrar Perfil</h1>
+            <div class="juntar-input">
 
-                <div class="juntar-input">
-
-                    <div class="input-field">
-                        <input class="input" type="text" name="nome" placeholder="Nome do Perfil" />
-                    </div>
-
+                <div class="input-field">
+                    <input class="input" type="text" name="nome" placeholder="Nome do Perfil" />
                 </div>
 
-
-                <h1 id="txt-perm">Permissões</h1>
-                <section class="perm-chama">
-
-                    <h1 id="txt-cad">Chamados:</h1>
-
-                    <div class="juntar-check">
-                        <label class="container-check">
-                            <input type="checkbox" name="cadastrar_chamado"/>
-                            <div for="">Cadastrar Chamado</div>
-                        </label>
-
-                        <label class="container-check">
-                            <input type="checkbox" name="todas_os"/>
-                            <div for="">Todos Chamados</div>
-                        </label>
-
-                        <label class="container-check">
-                            <input type="checkbox" name="requisicao_chamado"/>
-                            <div for="">Requisições de Chamados</div>
-                        </label>
+            </div>
 
 
-                        <label class="container-check">
-                            <input type="checkbox" name="relatorio_chamado"/>
-                            <div for="">Relatório de Chamado</div>
-                        </label>
+            <h1 id="txt-perm">Permissões</h1>
+            <section class="perm-chama">
 
-                        <label class="container-check">
-                            <input type="checkbox" name="aceitar_recusar_chamado"/>
-                            <div for="">Aceitar/recusar Chamado</div>
-                        </label>
+                <h1 id="txt-cad">Chamados:</h1>
 
-                        <label class="container-check">
-                            <input type="checkbox" name="editar_chamado"/>
-                            <div for="">Editar Chamado</div>
-                        </label>
-                    </div>
+               
+                    <label class="container-check">
+                        <input type="checkbox" name="cadastrar_chamado" value="1">
+                        <div for="">Cadastrar Chamado</div>
+                    </label>
 
-                </section>
+                    <label class="container-check">
+                        <input type="checkbox" name="todas_os" value="1">
+                        <div for="">Todos Chamados</div>
+                    </label>
 
-                <section class="perm-chama">
-
-                    <h1 id="txt-cad">OS:</h1>
-
-
-                    <div class="juntar-check">
-
-                        <label class="container-check">
-                            <input type="checkbox" name="relatorio_os"/>
-                            <div for="">Relatório de OS</div>
-                        </label>
-
-                        <label class="container-check">
-                            <input type="checkbox" name="responder_os"/>
-                            <div for="">Responder OS</div>
-                        </label>
-
-                        <label class="container-check">
-                            <input type="checkbox" name="aceitar_recusar_os"/>
-                            <div for="">Aceitar/recusar OS</div>
-                        </label>
-
-                        <label class="container-check">
-                            <input type="checkbox" name="editar_os"/>
-                            <div for="">Editar OS</div>
-                        </label>
-                    </div>
-
-                </section>
-
-                <section class="perm-cad">
-
-                    <h1 id="txt-cad">Cadastros:</h1>
-
-                    <div class="juntar-check">
-                        <label class="container-check">
-                            <input type="checkbox">
-                            <div for="">Cadastrar Chamado</div>
-                        </label>
-
-                        <label class="container-check">
-                            <input type="checkbox" name="cadastrar_usuario"/>
-                            <div for="">Cadastrar Usuário</div>
-                        </label>
-                    </div>
-
-                    <div class="juntar-check">
-                        <label class="container-check">
-                            <input type="checkbox" name="cadastrar_perfil"/>
-                            <div for="">Cadastrar Perfil</div>
-                        </label>
-
-                        <label class="container-check">
-                            <input type="checkbox" name="cadastrar_item"/>
-                            <div for="">Cadastrar Item</div>
-                        </label>
-                    </div>
-
-                    <div class="juntar-check">
-                        <label class="container-check">
-                            <input type="checkbox" name="cadastrar_cliente"/>
-                            <div for="">Cadastrar cliente</div>
-                        </label>
-                    </div>
+                    <label class="container-check">
+                        <input type="checkbox" name="requisicao_chamado" value="1">
+                        <div for="">Requisições de Chamados</div>
+                    </label>
 
 
-                </section>
+                    <label class="container-check">
+                        <input type="checkbox" name="relatorio_chamados" value="1">
+                        <div for="">Relatório de Chamado</div>
+                    </label>
+
+                    <label class="container-check">
+                        <input type="checkbox" name="aceitar_recusar_chamado" value="1">
+                        <div for="">Aceitar/recusar Chamado</div>
+                    </label>
+
+                    <label class="container-check">
+                        <input type="checkbox" name="editar_chamado" value="1">
+                        <div for="">Editar Chamado</div>
+                    </label>
+                
+
+            </section>
+
+            <section class="perm-chama">
+
+                <h1 id="txt-cad">OS:</h1>
 
 
-                <section class="perm-adm">
+                
 
-                    <h1 id="txt-cad">Administrativo:</h1>
+                    <label class="container-check">
+                        <input type="checkbox" name="relatorio_os" value="1">
+                        <div for="">Relatório de OS</div>
+                    </label>
 
-                    <div class="juntar-check">
-                        <label class="container-check">
-                            <input type="checkbox" name="gerenciar_usuario"/>
-                            <div for="">Gerenciar Usuário</div>
-                        </label>
+                    <label class="container-check">
+                        <input type="checkbox" name="responder_os" value="1">
+                        <div for="">Responder OS</div>
+                    </label>
 
-                        <label class="container-check">
-                            <input type="checkbox" name="gerenciar_perfil"/>
-                            <div for="">Gerenciar Perfil</div>
-                        </label>
-                    </div>
+                    <label class="container-check">
+                        <input type="checkbox" name="aceitar_recusar_os" value="1">
+                        <div for="">Aceitar/recusar OS</div>
+                    </label>
 
-                    <div class="juntar-check">
-                        <label class="container-check">
-                            <input type="checkbox" name="gerenciar_item"/>
-                            <div for="">Gerenciar Item</div>
-                        </label>
+                    <label class="container-check">
+                        <input type="checkbox" name="editar_os" value="1">
+                        <div for="">Editar OS</div>
+                    </label>
+                
 
-                        <label class="container-check">
-                            <input type="checkbox" name="gerenciar_cliente"/>
-                            <div for="">Gerenciar Cliente</div>
-                        </label>
-                    </div>
+            </section>
 
-                </section>
+            <section class="perm-cad">
+
+                <h1 id="txt-cad">Cadastros:</h1>
+
+                
+                    <label class="container-check">
+                        <input type="checkbox" name="cadastrar_usuario" value="1">
+                        <div for="">Cadastrar Usuário</div>
+                    </label>
+        
+                    <label class="container-check">
+                        <input type="checkbox" name="cadastrar_perfil" value="1">
+                        <div for="">Cadastrar Perfil</div>
+                    </label>
+           
+                    <label class="container-check">
+                        <input type="checkbox" name="cadastrar_item" value="1">
+                        <div for="">Cadastrar Item</div>
+                    </label>
+             
+                    <label class="container-check">
+                        <input type="checkbox" name="cadastrar_cliente" value="1">
+                        <div for="">Cadastrar cliente</div>
+                    </label>
+              
 
 
-
-                <div class="btn-field">
-
-                    <button class="btn-submit" type="submit">Cadastrar</button>
-                    <a href="" class="btn-cancelar" id="cancelar">Cancelar</a href="">
-
-                </div>
+            </section>
 
 
-            </form>
+            <section class="perm-adm">
+
+                <h1 id="txt-cad">Administrativo:</h1>
+
+                
+                    <label class="container-check">
+                        <input type="checkbox" name="gerenciar_usuario" value="1">
+                        <div for="">Gerenciar Usuário</div>
+                    </label>
+           
+                    <label class="container-check">
+                        <input type="checkbox" name="gerenciar_perfil" value="1">
+                        <div for="">Gerenciar Perfil</div>
+                    </label>
+          
+                    <label class="container-check">
+                        <input type="checkbox" name="gerenciar_item" value="1">
+                        <div for="">Gerenciar Item</div>
+                    </label>
+             
+
+               
+                    <label class="container-check">
+                        <input type="checkbox" name="gerenciar_cliente" value="1">
+                        <div for="">Gerenciar Cliente</div>
+                    </label>
+               
+
+            </section>
+
+            <div class="btn-field">
+
+                <button class="btn-submit" type="submit">Cadastrar</button>
+                <a href="" class="btn-cancelar" id="cancelar">Cancelar</a>
+
+            </div>
 
 
-        </section>
+        </form>
+
+
+    </section>
 
 
 
-    </body>
-
-    </html>
 </body>
 
 </html>
