cdeeca056019e357a8a53d727ac52c6eacac0c39
Igor <igoralbieri04@gmail.com>
2024-06-12 13:23:17 -0400

    a

--- includes/php/suas_os.php
+++ includes/php/suas_os.php
@@ -7,105 +7,149 @@ use App\Entity\Usuario;
 if (isset($_SESSION['id_user'])) {
     // Buscar as informações do usuário no banco de dados usando o ID da sessão
     $objUsuario = Usuario::getUser($_SESSION['id_user']);
+    // Obter permissões do usuário
+    $dados = $objUsuario->getPermissao($_SESSION['id_user']);
+    $resp_os = $dados->resp_os == '1';
+    $edit_os = $dados->edit_os == '1';
 }
 
 $dados = Chamado::getChama();
 
-$user_lista = '';
-$user_table = '';
+$user_lista = ''; // Inicializa a variável
+$user_table = ''; // Inicializa a variável
 
 foreach ($dados as $user) {
-
     $isResponsible = $user['id_user'] == $objUsuario->id_user;
     $isSolicitante = $user['solicitante'] == $objUsuario->id_user;
 
-    if (($user['status'] == "os" || $user['status'] == "os_respondida" || $user['status'] == "os_finalizada") && ($isResponsible || $isSolicitante)) {
-        $prioridade = strtolower($user['prioridade']);
-        $cardClass = "card1";
-        $notiGlowClass = "notiglow1";
-        $notiBorderGlowClass = "notiborderglow1";
-        $notiTitleClass = "notititle1";
-        $notiBodyClass = "notibody1";
+    $card_class = '';
+    $glow_class = '';
+    $border_glow_class = '';
+    $title_class = '';
+    $body_class = '';
 
-        if ($prioridade == "media") {
-            $cardClass = "card3";
-            $notiGlowClass = "notiglow3";
-            $notiBorderGlowClass = "notiborderglow3";
-            $notiTitleClass = "notititle3";
-            $notiBodyClass = "notibody3";
-        } elseif ($prioridade == "alta") {
-            $cardClass = "card2";
-            $notiGlowClass = "notiglow2";
-            $notiBorderGlowClass = "notiborderglow2";
-            $notiTitleClass = "notititle2";
-            $notiBodyClass = "notibody2";
+    // Define classes based on priority
+    if ($user['prioridade'] == "baixa") {
+        $card_class = 'card1';
+        $glow_class = 'notiglow1';
+        $border_glow_class = 'notiborderglow1';
+        $title_class = 'notititle1';
+        $body_class = 'notibody1';
+    } elseif ($user['prioridade'] == "media") {
+        $card_class = 'card3';
+        $glow_class = 'notiglow3';
+        $border_glow_class = 'notiborderglow3';
+        $title_class = 'notititle3';
+        $body_class = 'notibody3';
+    } elseif ($user['prioridade'] == "alta") {
+        $card_class = 'card2';
+        $glow_class = 'notiglow2';
+        $border_glow_class = 'notiborderglow2';
+        $title_class = 'notititle2';
+        $body_class = 'notibody2';
+    }
+
+    // Add cards based on status
+    if (($user['status'] == "os" || $user['status'] == "os_respondida") && ($isResponsible || $isSolicitante)) {
+        $action_button = $user['status'] == "os" ? 'Responder' : 'Visualizar';
+        $action_url = $user['status'] == "os" ? 'main_validar_os.php' : 'main_validar_os.php';
+
+        $user_lista .= "
+            <div class=\"$card_class\">
+                <div class=\"$glow_class\"></div>
+                <div class=\"$border_glow_class\"></div>
+                <div class=\"$title_class\">Chamado {$user['id_chamado']}</div>
+                <div class=\"$body_class\">
+                    Requisitante: {$user['nome_solicitante']}<br>
+                    Abertura: {$user['abertura']}<br><br>
+                    Equipamento(s): {$user['nome_equip']}<br>
+                    Tipo: {$user['tipo']}<br>
+                    Prioridade: {$user['prioridade']}<br><br>
+                    Descrição: {$user['descricao']}<br><br>
+                    Responsável: {$user['nome_resp']}<br>
+                    Cliente: {$user['nome_cliente']}<br>";
+                    
+        if ($resp_os) {
+            $user_lista .= "<a href=\"../pages/$action_url?id_chamado={$user['id_chamado']}\"><button type=\"button\" class=\"btn btn-primary\" name=\"responder\" id=\"btnAceitar\">Responder</button></a>";
         }
 
-        $user_lista .= '
-        <div class="' . $cardClass . '">
-            <div class="' . $notiGlowClass . '"></div>
-            <div class="' . $notiBorderGlowClass . '"></div>
-            <div class="' . $notiTitleClass . '">Chamado ' . $user['id_chamado'] . '</div>
-            <div class="' . $notiBodyClass . '">
-                Requisitante: ' . $user['nome_solicitante'] . '<br>
-                Abertura: ' . $user['abertura'] . '<br><br>
-                Equipamento(s): ' . $user['nome_equip'] . '<br>
-                Tipo: ' . $user['tipo'] . '<br>
-                Prioridade: ' . $user['prioridade'] . '<br><br>
-                Descrição: ' . $user['descricao'] . '<br><br>
-                Responsável: ' . $user['nome_resp'] . '<br>
-                Cliente: ' . $user['nome_cliente'] . '<br>';
-
-        if ($user['status'] == "os") {
-            $user_lista .= '
-                <a href="../includes/php/aceitar_chamado.php?id_chamado=' . $user['id_chamado'] . '"><button type="button" class="btn btn-primary" name="responder" id="btnResponder">Responder</button></a>
-                <a href="../pages/main_editar_chama.php?id_chamado=' . $user['id_chamado'] . '"><button type="button" class="btn btn-dark">Editar</button></a>
-                <button type="button" class="btn btn-danger">Desativar</button>';
-        } elseif ($user['status'] == "os_respondida") {
-            $user_lista .= '
-                <a href="../pages/main_visualizar_chama.php?id_chamado=' . $user['id_chamado'] . '"><button type="button" class="btn btn-primary" name="visualizar" id="btnVisualizar">Visualizar</button></a>
-                <a href="../pages/main_editar_chama.php?id_chamado=' . $user['id_chamado'] . '"><button type="button" class="btn btn-dark">Editar</button></a>';
-        } elseif ($user['status'] == "os_finalizada") {
-            $user_lista .= '
-                <a href="../pages/main_visualizar_chama.php?id_chamado=' . $user['id_chamado'] . '"><button type="button" class="btn btn-primary" name="visualizar" id="btnVisualizar">Visualizar</button></a>
-                <a href="../pages/main_editar_chama.php?id_chamado=' . $user['id_chamado'] . '"><button type="button" class="btn btn-dark">Editar</button></a>';
+        if ($edit_os) {
+            $user_lista .= "<a href=\"../pages/main_editar_chama.php?id_chamado={$user['id_chamado']}\"><button type=\"button\" class=\"btn btn-dark\">Editar</button></a>";
         }
 
-        $user_lista .= '
-            </div>
-        </div>';
+        $user_lista .= "</div></div>";
+    }
 
-        // Limitar a descrição para 140 caracteres
+    if (($user['status'] == "os_finalizada") && ($isResponsible || $isSolicitante)) {
+        $action_url = 'main_visualizar_os.php';
+
+        $user_lista .= "
+            <div class=\"$card_class\">
+                <div class=\"$glow_class\"></div>
+                <div class=\"$border_glow_class\"></div>
+                <div class=\"$title_class\">Chamado {$user['id_chamado']}</div>
+                <div class=\"$body_class\">
+                    Requisitante: {$user['nome_solicitante']}<br>
+                    Abertura: {$user['abertura']}<br><br>
+                    Equipamento(s): {$user['nome_equip']}<br>
+                    Tipo: {$user['tipo']}<br>
+                    Prioridade: {$user['prioridade']}<br><br>
+                    Descrição: {$user['descricao']}<br><br>
+                    Responsável: {$user['nome_resp']}<br>
+                    Cliente: {$user['nome_cliente']}<br>";
+                    
+        $user_lista .= "<a href=\"../pages/$action_url?id_chamado={$user['id_chamado']}\"><button type=\"button\" class=\"btn btn-primary\" name=\"responder\" id=\"btnAceitar\">Visualizar</button></a>";
+        
+        $user_lista .= "</div></div>";
+    }
+
+    // Add rows to table
+    if (($user['status'] == "os" || $user['status'] == "os_respondida")&& ($isResponsible || $isSolicitante)) {
         $descricao = (!empty($user['descricao']) ? (strlen($user['descricao']) > 140 ? substr($user['descricao'], 0, 140) . '...' : $user['descricao']) : 'campo vazio');
 
-        $user_table .= '
-        <tr>
-            <td>' . (!empty($user['id_chamado']) ? $user['id_chamado'] : 'campo vazio') . '</td>
-            <td>' . (!empty($user['nome_solicitante']) ? $user['nome_solicitante'] : 'campo vazio') . '</td>
-            <td>' . (!empty($user['abertura']) ? $user['abertura'] : 'campo vazio') . '</td>
-            <td>' . (!empty($user['nome_equip']) ? $user['nome_equip'] : 'campo vazio') . '</td>
-            <td>' . (!empty($user['tipo']) ? $user['tipo'] : 'campo vazio') . '</td>
-            <td>' . (!empty($user['prioridade']) ? $user['prioridade'] : 'campo vazio') . '</td>
-            <td>' . $descricao . '</td>
-            <td>' . (!empty($user['nome_resp']) ? $user['nome_resp'] : 'campo vazio') . '</td>
-            <td>' . (!empty($user['nome_cliente']) ? $user['nome_cliente'] : 'campo vazio') . '</td>
-            <td>';
+        $user_table .= "
+            <tr>
+                <td>" . (!empty($user['id_chamado']) ? $user['id_chamado'] : 'campo vazio') . "</td>
+                <td>" . (!empty($user['nome_solicitante']) ? $user['nome_solicitante'] : 'campo vazio') . "</td>
+                <td>" . (!empty($user['abertura']) ? $user['abertura'] : 'campo vazio') . "</td>
+                <td>" . (!empty($user['nome_equip']) ? $user['nome_equip'] : 'campo vazio') . "</td>
+                <td>" . (!empty($user['tipo']) ? $user['tipo'] : 'campo vazio') . "</td>
+                <td>" . (!empty($user['prioridade']) ? $user['prioridade'] : 'campo vazio') . "</td>
+                <td>$descricao</td>
+                <td>" . (!empty($user['nome_resp']) ? $user['nome_resp'] : 'campo vazio') . "</td>
+                <td>" . (!empty($user['nome_cliente']) ? $user['nome_cliente'] : 'campo vazio') . "</td>
+                <td>";
 
-        if ($user['status'] == "os") {
-            $user_table .= '
-                <a href="../includes/php/aceitar_chamado.php?id_chamado=' . $user['id_chamado'] . '"><button type="button" class="btn btn-primary" name="responder" id="btnResponder">Responder</button></a>
-                <a href="../pages/main_editar_chama.php?id_chamado=' . $user['id_chamado'] . '"><button type="button" class="btn btn-dark">Editar</button></a>
-                <button type="button" class="btn btn-danger">Desativar</button>';
-        } elseif ($user['status'] == "os_respondida" || $user['status'] == "os_finalizada") {
-            $user_table .= '
-                <a href="../pages/main_visualizar_chama.php?id_chamado=' . $user['id_chamado'] . '"><button type="button" class="btn btn-primary" name="visualizar" id="btnVisualizar">Visualizar</button></a>
-                <a href="../pages/main_editar_chama.php?id_chamado=' . $user['id_chamado'] . '"><button type="button" class="btn btn-dark">Editar</button></a>';
+        if ($resp_os) {
+            $user_table .= "<a href=\"../pages/$action_url?id_chamado={$user['id_chamado']}\"><button type=\"button\" class=\"btn btn-primary\" name=\"responder\" id=\"btnAceitar\">Responder</button></a>";
         }
 
-        $user_table .= '
-            </td>
-        </tr>';
+        if ($edit_os) {
+            $user_table .= "<a href=\"../pages/main_editar_chama.php?id_chamado={$user['id_chamado']}\"><button type=\"button\" class=\"btn btn-dark\">Editar</button></a>";
+        }
+
+        $user_table .= "</td></tr>";
+    }
+
+    if (($user['status'] == "os_finalizada") && ($isResponsible || $isSolicitante)) {
+        $descricao = (!empty($user['descricao']) ? (strlen($user['descricao']) > 140 ? substr($user['descricao'], 0, 140) . '...' : $user['descricao']) : 'campo vazio');
+
+        $user_table .= "
+            <tr>
+                <td>" . (!empty($user['id_chamado']) ? $user['id_chamado'] : 'campo vazio') . "</td>
+                <td>" . (!empty($user['nome_solicitante']) ? $user['nome_solicitante'] : 'campo vazio') . "</td>
+                <td>" . (!empty($user['abertura']) ? $user['abertura'] : 'campo vazio') . "</td>
+                <td>" . (!empty($user['nome_equip']) ? $user['nome_equip'] : 'campo vazio') . "</td>
+                <td>" . (!empty($user['tipo']) ? $user['tipo'] : 'campo vazio') . "</td>
+                <td>" . (!empty($user['prioridade']) ? $user['prioridade'] : 'campo vazio') . "</td>
+                <td>$descricao</td>
+                <td>" . (!empty($user['nome_resp']) ? $user['nome_resp'] : 'campo vazio') . "</td>
+                <td>" . (!empty($user['nome_cliente']) ? $user['nome_cliente'] : 'campo vazio') . "</td>
+                <td>";
+
+        $user_table .= "<a href=\"../pages/main_visualizar_os.php?id_chamado={$user['id_chamado']}\"><button type=\"button\" class=\"btn btn-primary\" name=\"responder\" id=\"btnAceitar\">Visualizar</button></a>";
+
+        $user_table .= "</td></tr>";
     }
 }
-
 ?>
